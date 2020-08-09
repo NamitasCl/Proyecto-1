b400a4a6e5a4d944a02cfbe419c444d116cd40ba
NamitasCl <63020574+NamitasCl@users.noreply.github.com>
2020-08-09 17:17:06 -0400

    Cambios realizados
    Se actualizo el boton share

---
+++ scripts/js.js
@@ -0,0 +1,8 @@
+const share = document.getElementById("user__share");
+const oculto = document.getElementById("oculto");
+
+share.addEventListener('click', () => {
+    
+    oculto.classList.toggle('oculto');
+
+});
